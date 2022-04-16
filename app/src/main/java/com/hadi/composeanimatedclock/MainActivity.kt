3b03278f8775d4e36d2f8523d562f87b9695412c
Unaisul Hadi <unaisulhadi@gmail.com>
2022-04-16 12:18:07 +0530

    🚀 Basic UI set up Number and NumberColumn

--- app/src/main/java/com/hadi/composeanimatedclock/MainActivity.kt
+++ app/src/main/java/com/hadi/composeanimatedclock/MainActivity.kt
@@ -3,13 +3,23 @@ package com.hadi.composeanimatedclock
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.hadi.composeanimatedclock.ui.theme.ComposeAnimatedClockTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Number(3, false)
                 }
             }
         }
@@ -30,14 +40,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Number(value: Int, active: Boolean) {
+    val backgroundColor = if (active) {
+        MaterialTheme.colors.primary
+    } else {
+        MaterialTheme.colors.primaryVariant
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .size(40.dp)
+            .background(backgroundColor),
+    ) {
+        Text(
+            text = value.toString(),
+            fontSize = 20.sp,
+            color = Color.White,
+        )
+    }
+}
+
+@Composable
+fun NumberColumn(
+    range: IntRange,
+    current: Int
+) {
+
+    Column(
+        modifier = Modifier.clip(RoundedCornerShape(percent = 25))
+    ) {
+        range.forEach { num ->
+            Number(value = num, active = num == current)
+        }
+    }
+
+}
+
+
+@Composable
+@Preview
+fun NumberColumnPreview() {
+    NumberColumn(range = 0..9, current = 5)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeAnimatedClockTheme {
-        Greeting("Android")
+    Column {
+        Number(value = 3, active = true)
+        Number(value = 7, active = false)
     }
 }
